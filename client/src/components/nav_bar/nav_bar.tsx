0122501b8a55a51ac04539e4fd7a5d74266b8cc5
George Madeley <prof.chewbacca@gmail.com>
2023-03-01 17:53:52 +0000

    Improved Nav Bar Response

--- client/src/components/nav_bar/nav_bar.tsx
+++ client/src/components/nav_bar/nav_bar.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
+
 import './nav_bar.css';
+import nav_json from './nav_bar.json';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -8,55 +10,46 @@ import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import CustomButton from '../custom_button/custom_button';
 
 export default function NavBar() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isNavOpen, setNavOpen] = useState(false);
+  const navRef = useRef<HTMLUListElement>(null);
+  const nav_buttons = nav_json.nav_buttons;
+
+  const closeNav = (e: any) => {
+    if (navRef.current && isNavOpen && !navRef.current.contains(e.target)) {
+      setNavOpen(false);
+    }
+  };
+
+  window.addEventListener('click', closeNav);
 
   const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
+    setNavOpen(!isNavOpen);
   };
 
   return (
-    <nav>
+    <nav ref={navRef}>
       <div className='upper-nav'>
         <div className='logo'>
           <Link className='link' to='/'>Biodevices without Borders</Link>
         </div>
-        <div className={`burger cross-${isNavOpen}`}>
+        <div className={`burger cross-${isNavOpen}`} onClick={toggleNav}>
           {
-            isNavOpen ? <FontAwesomeIcon icon={faXmark} onClick={toggleNav} /> : <FontAwesomeIcon icon={faBars} onClick={toggleNav} />
+            isNavOpen ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />
           }
         </div>
       </div>
       <ul className={`nav-list nav-list-${isNavOpen}`}>
-        <li id='about-button'>
-          <CustomButton lnIntFraction={0/5}>
-            <Link className='link' to='about'>About</Link>
-          </CustomButton>
-        </li>
-        <li id='donate-button'>
-          <CustomButton lnIntFraction={1/5}>
-            <Link className='link' to='donate'>Donate</Link>
-          </CustomButton>
-        </li>
-        <li id='future-button'>
-          <CustomButton lnIntFraction={2/5}>
-            <Link className='link' to='future'>Future Plans</Link>
-          </CustomButton>
-        </li>
-        <li id='issues-button'>
-          <CustomButton lnIntFraction={3/5}>
-            <Link className='link' to='issue'>Issues</Link>
-          </CustomButton>
-        </li>
-        <li id='partners-button'>
-          <CustomButton lnIntFraction={4/5}>
-            <Link className='link' to='partners'>Partners</Link>
-          </CustomButton>
-        </li>
-        <li id='progress-button'>
-          <CustomButton lnIntFraction={5/5}>
-            <Link className='link' to='progress'>Progress</Link>
-          </CustomButton>
-        </li>
+        {
+          nav_buttons.map((button, index) => {
+            return (
+              <li key={index} id={`${button.id}-button`}>
+                <CustomButton lnIntFraction={index/nav_buttons.length}>
+                  <Link className='link' to={button.link}>{button.text}</Link>
+                </CustomButton>
+              </li>
+            )
+          })
+        }
       </ul>
     </nav>
   )
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -19,7 +19,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <Router>
       <Routes>
         <Route path='/' element={<App />}>
@@ -39,7 +38,6 @@ root.render(
         </Route>
       </Routes>
     </Router>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
