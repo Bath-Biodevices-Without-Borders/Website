d229bb34538d9bfb9833953f0cc3f37b410e1243
George Madeley <prof.chewbacca@gmail.com>
2023-03-06 11:34:54 +0000

    Added About Page Highlight Box

---
+++ client/src/components/highlight_box/highlight_box.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './highlight_box.css';
+
+import team_img_1 from '../../images/temp_team_1.jpg';
+import team_img_2 from '../../images/temp_team_2.jpg';
+import team_img_3 from '../../images/temp_team_3.jpg';
+
+type T_highlight_card = {id: string, title: string, text: string, img: string, alt: string};
+
+export default function HighlightBox(props: {cards: T_highlight_card[]}) {
+
+  const images = [team_img_1, team_img_2, team_img_3];
+  const cardWidth = `${100 / props.cards.length}%`;
+
+  return (
+    <div className='highlight-box'>
+      <div className='card-grid'>
+        {
+          props.cards.map((card: T_highlight_card, index: number) => {
+            return (
+              <div key={index} id={`${card.id}-card`} className='card' style={{ width: cardWidth }}>
+                <img src={images[index]} alt={card.alt} />
+                <div className='content'>
+                  <h2>{card.title}</h2>
+                  <p>{card.text}</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+}
--- client/src/pages/about_page/about_page.tsx
+++ client/src/pages/about_page/about_page.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+
 import './about_page.css';
+import about_json from './about_page.json';
+
+import HighlightBox from '../../components/highlight_box/highlight_box';
 
 /**
  * @abstract A page consisting of information about the organization.
@@ -10,6 +14,9 @@ export default function AboutPage() {
   return (
     <div className="about-page">
       <h1>About Page</h1>
+      <div className='hero'>
+        <HighlightBox cards={about_json.heroCards}/>
+      </div>
     </div>
   );
 }
