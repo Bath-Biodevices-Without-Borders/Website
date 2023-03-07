4c433f71943dffa46beca26a4239e1be9fbfa52f
George Madeley <prof.chewbacca@gmail.com>
2023-03-07 15:26:16 +0000

    Added About Page

--- client/src/pages/about_page/about_page.tsx
+++ client/src/pages/about_page/about_page.tsx
@@ -5,18 +5,66 @@ import about_json from './about_page.json';
 
 import HighlightBox from '../../components/highlight_box/highlight_box';
 
+import imgPerson1 from '../../images/temp_person_1.jpg';
+import imgPerson2 from '../../images/temp_person_2.jpg';
+import imgPerson3 from '../../images/temp_person_3.jpg';
+import imgPerson4 from '../../images/temp_person_4.jpg';
+
+type T_member = {
+  name: string,
+  title: string,
+  text: string,
+  imgIdx: number,
+  alt: string
+}
+
+type T_team = {
+  id: string,
+  name: string,
+  text: string,
+  members: T_member[]
+}
+
 /**
  * @abstract A page consisting of information about the organization.
  * 
  * @returns the about page
  */
 export default function AboutPage() {
+
+  const profileImages: any[] = [imgPerson1, imgPerson2, imgPerson3, imgPerson4];
+
   return (
     <div className="about-page">
-      <h1>About Page</h1>
       <div className='hero'>
+        <h1>About Us</h1>
         <HighlightBox cards={about_json.heroCards}/>
       </div>
+      {
+        about_json.teams.map((team: T_team, index: number) => {
+          return (
+            <section key={index}>
+              <div className='divider' id={team.id}></div>
+              <h2>{team.name}</h2>
+              <p>{team.text}</p>
+              <div className='members'>
+                {
+                  team.members.map((member, index) => {
+                    return(
+                      <div key={index} className='member'>
+                        <img src={profileImages[member.imgIdx]} alt={member.alt}/>
+                        <h3>{member.name}</h3>
+                        <h4>{member.title}</h4>
+                        <p>{member.text}</p>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </section>
+          )
+        })
+      }
     </div>
   );
 }
