e98d6415707a639b6fb700597f07bd25f45929d9
George Madeley <george.madeley@outlook.com>
2023-08-23 23:03:28 +0100

    Removed Variables Script

--- src/components/custom_button/custom_button.tsx
+++ src/components/custom_button/custom_button.tsx
@@ -1,15 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { colors, DiagonalBackground } from '../../variables';
+import React from 'react';
 import './custom_button.css';
 
 export default function CustomButton(props: { children: any, lnIntFraction: number }) {
-  const [background, setBackground] = useState({})
-
-  useEffect(() => {
-    const hue = ((Math.abs(colors[0].h - colors[4].h) * props.lnIntFraction) + colors[4].h) % 360;
-    setBackground(DiagonalBackground({ h: hue, s: 100, l: 25 }, { h: hue, s: 100, l: 75 }));
-  }, [props.lnIntFraction])
-
   return (
     <div className='custom-button'>
       <div className='flex'>
