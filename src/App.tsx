466a87de572c1eac0d197bc2529b03af351a5840
George Thomas Madeley <george.madeley@outlook.com>
2024-02-06 18:10:49 +0000

    Changed To Hash Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
 import './App.css';
 import NavBar from './components/nav_bar/nav_bar';
 import Footer from './components/footer/footer';
-import HomePage from './pages/home_page/home_page';
-import AboutPage from './pages/about_page/about_page';
-import OasisPage from './pages/oasis_page/oasis_page';
-import ContactPage from './pages/contact_page/contact_page';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          <header>
-            <NavBar />
-            <Routes>
-              <Route path="/" element={<HomePage />}></Route>
-              <Route path="/about" element={<AboutPage />}></Route>
-              <Route path="/oasis" element={<OasisPage/>}></Route>
-              <Route path="/contact" element={<ContactPage/>}></Route>
-            </Routes>
-          </header>
-          <Footer />
-        </Router>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css'
+
+import App from './App';
+import HomePage from './pages/home_page/home_page';
+import AboutPage from './pages/about_page/about_page';
+import OasisPage from './pages/oasis_page/oasis_page';
+import ContactPage from './pages/contact_page/contact_page';
+
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/about', element: <AboutPage /> },
+      { path: '/oasis', element: <OasisPage /> },
+      { path: '/contact', element: <ContactPage /> }
+    ]
+  }
+])
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById('root')
 );
