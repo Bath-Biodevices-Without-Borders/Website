bc474f0100906c96db31e675bfa30e8b8a66da17
George Thomas Madeley <george.madeley@outlook.com>
2024-03-26 11:24:01 +0000

    Abstracted Paragraphs to JSON

--- src/pages/about_page/our_story/our_story.tsx
+++ src/pages/about_page/our_story/our_story.tsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import './our_story.css'
 
+import aboutPageJson from '../../../content/about_page.json';
+
 export default function OurStory() {
   return (
     <div className="card-product">
       <div className="card-product-infos">
         <h2>Our Story</h2>
-        <br></br>
-        <p>Team Bath Biodevices is a Vertically Integrated Project at the Univeristy of Bath formed by a diverse and
-          multi-disciplinary team that aims to create
-          <strong> portable</strong> and <strong>affordable</strong> electrochemical devices that enable fast and reliable water
-          quality quantification.</p>
-        <br></br>
-        <p> At the core of our mission, we are dedicated to address a global challenge: the scarcity of reliable and clean water in
-          Sub-Saharan Africa. We believe that access to clean water is a fundamental human right, and our team is driven to make this right a reality for communities facing water scarcity.
-          Every step we take, every innovation we make, is fueled bythe desire to make a meaningful difference in the lives of those who lack reliable water supplies.
-        </p>
-        <br></br>
-        <p>
-          Join us on this incredible journey as we work towards a world
-          where clean water is not a luxury but a basic necessity accessible to all.
-          </p>
+        {
+          aboutPageJson.ourStory.map((paragraph: string, index: number) => {
+            return (
+              <p>{paragraph}</p>
+            )
+          })
+        }
       </div>
     </div>
   )
