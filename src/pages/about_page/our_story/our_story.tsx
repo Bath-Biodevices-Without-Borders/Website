dc5f0595ad2f012701659766d4a491e947123336
George Thomas Madeley <george.madeley@outlook.com>
2024-03-26 11:30:11 +0000

    Adjusted the Style for Our Story

--- src/pages/about_page/our_story/our_story.tsx
+++ src/pages/about_page/our_story/our_story.tsx
@@ -5,17 +5,15 @@ import aboutPageJson from '../../../content/about_page.json';
 
 export default function OurStory() {
   return (
-    <div className="card-product">
-      <div className="card-product-infos">
-        <h2>Our Story</h2>
-        {
-          aboutPageJson.ourStory.map((paragraph: string, index: number) => {
-            return (
-              <p>{paragraph}</p>
-            )
-          })
-        }
-      </div>
+    <div className="our-story">
+      <h2>Our Story</h2>
+      {
+        aboutPageJson.ourStory.map((paragraph: string, index: number) => {
+          return (
+            <p>{paragraph}</p>
+          )
+        })
+      }
     </div>
   )
 }
