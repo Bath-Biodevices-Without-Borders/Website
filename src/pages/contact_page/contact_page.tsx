a515c07c852a86433d64ca7d236a12f2152d501f
George Thomas Madeley <george.madeley@outlook.com>
2024-04-03 14:40:03 +0100

    Updated Get In Touch Page Responsiveness

--- src/pages/contact_page/contact_page.tsx
+++ src/pages/contact_page/contact_page.tsx
@@ -3,14 +3,7 @@ import './contact_page.css';
 export default function ContactPage() {
   return (
     <div className="contact-page">
-      <div
-        className="card-category"
-        style={{
-          backgroundImage:
-            'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.3)), url(https://images.unsplash.com/photo-1511632765486-a01980e01a18?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          backgroundPosition: '20% 30%'
-          }}
-      >
+      <div className="hero">
         Get in touch
       </div>
       <div className='form-container'>
