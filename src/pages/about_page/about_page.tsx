0ea83f28b3b4b638a50773968d6015de326473a3
George Thomas Madeley <george.madeley@outlook.com>
2024-05-23 10:09:09 +0100

    Added Opacity Nav Animation to All Pages

--- src/pages/about_page/about_page.tsx
+++ src/pages/about_page/about_page.tsx
@@ -1,12 +1,24 @@
+import { useContext, useEffect, useRef } from 'react';
 import './about_page.css';
-import OurStory from './our_story/our_story';
 
+import OurStory from './our_story/our_story';
 import TeamList from './team_profiles';
 
+import { HeroContext } from '../../context/hero_context';
+
 export default function AboutPage() {
+  const { setHeroRef } = useContext(HeroContext);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      setHeroRef(ref);
+    }
+  }, [ref, setHeroRef]);
+
   return (
     <div className="about-page">
-      <div className="hero">
+      <div className="hero" ref={ref}>
         About Us
       </div>
       <OurStory />
