9ec0f74699410dd731db6fa9326c91180eaded73
George Madeley <george.madeley@outlook.com>
2024-05-27 19:01:56 +0100

    Created Join Us Form

---
+++ src/pages/join_us_page/form/form.tsx
@@ -0,0 +1,127 @@
+import React, { useEffect } from 'react'
+import './form.css'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import Checkbox from './checkbox/checkbox'
+import { I_FormInput, T_TeamStrings } from '../../../types/types'
+
+export default function Form() {
+  const teams: T_TeamStrings[] = [
+    'finance',
+    'hardware',
+    'outreach',
+    'sensors',
+    'social',
+    'software',
+  ]
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<I_FormInput>()
+
+  const onSubmit: SubmitHandler<I_FormInput> = data => {
+    console.log('data:', data);
+    console.log('errors:', errors);
+  }
+
+  useEffect(() => {
+    console.log(errors);
+  }, [errors])
+
+  const emailValidation = (email: string) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@bath.ac.uk$/
+    const isValid = emailRegex.test(email)
+    return isValid ? true : "Please enter a valid University of Bath email address."
+  }
+
+  const checkboxValidation = (teams: T_TeamStrings[] | boolean) => {
+    console.log('selected teams:');
+    console.log(teams);
+    if (typeof teams === 'boolean') {
+      return teams ? true : "Please select at least one team."
+    } else {
+      return teams.length > 0 ? true : "Please select at least one team."
+    }
+  }
+
+  return (
+    <div className='form-container'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <section>
+            <label htmlFor='first-name'>
+              <p className='label'>First Name</p>
+              {errors.firstName && <p className='required'>required</p>}
+            </label>
+            <input
+              className={errors.firstName ? 'required-error' : ''}
+              type='text'
+              placeholder='John'
+              {...register('firstName', { required: true })}
+            />
+          </section>
+          <section>
+            <label htmlFor='last-name'>
+              <p>Last Name</p>
+              {errors.lastName && <p className='required'>required</p>}
+            </label>
+            <input
+              className={errors.lastName ? 'required-error' : ''}
+              type='text'
+              placeholder='Doe'
+              {...register('lastName', { required: true })}
+            />
+          </section>
+        </div>
+        <section>
+          <label htmlFor='email'>
+            <p>University of Bath Email</p>
+            {errors.email && <p className='required'>required</p>}
+          </label>
+          {errors.email && <p className='error-message'>{errors.email.message}</p>}
+          <input
+            className={errors.email ? 'required-error' : ''}
+            type='email'
+            placeholder='abc123@bath.ac.uk'
+            {...register('email', { validate: emailValidation })}
+          />
+        </section>
+        <section>
+          <label htmlFor='team'>
+            <p>Team</p>
+            {errors.teams?.message && <p className='required'>required</p>}
+          </label>
+          {errors.email && <p className='error-message'>{errors.teams?.message}</p>}
+          <div className={`checkboxes ${errors.teams ? "required-error" : ""}`}>
+            {
+              teams.map((team : T_TeamStrings) => (
+                <Checkbox
+                  key={team}
+                  name='teams'
+                  id={team}
+                  register={register}
+                  validation={checkboxValidation}
+                >
+                  {team.charAt(0).toUpperCase() + team.slice(1)}
+                </Checkbox>
+              ))
+            }
+          </div>
+        </section>
+        <section>
+          <label htmlFor='message'>
+            <p>Message</p>
+            <p className='optional'>optional</p>
+          </label>
+          <p className='description'>Write a brief message detailing why you want to join Biodeivces Without Borders.</p>
+          <textarea {...register('message')}/>
+        </section>
+        <section>
+          <button type='submit'>Submit</button>
+        </section>
+      </form>
+    </div>
+  )
+}
--- src/pages/join_us_page/join_us_page.tsx
+++ src/pages/join_us_page/join_us_page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Hero from '../../components/hero/hero'
+import Form from './form/form'
 
 export default function JoinUsPage() {
   return (
-    <div className='join-us-page'>JoinUsPage</div>
+    <div className='join-us-page'>
+      <Hero useParallax={false} imageUrl='https://images.unsplash.com/photo-1510146758428-e5e4b17b8b6a?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
+        <h1>Join Us</h1>
+      </Hero>
+      <Form />
+    </div>
   )
 }
