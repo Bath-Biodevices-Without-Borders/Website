0e80ab3bfc4e164de57cdc4970938b37c1126a0c
George Madeley <george.madeley@outlook.com>
2024-05-27 22:03:05 +0100

    Added Introduction Section to Join Us Page

--- src/pages/join_us_page/join_us_page.tsx
+++ src/pages/join_us_page/join_us_page.tsx
@@ -4,6 +4,7 @@ import './join_us_page.css'
 
 import Hero from '../../components/hero/hero'
 import Form from './form/form'
+import Section from './section/section'
 
 export default function JoinUsPage() {
   return (
@@ -11,6 +12,7 @@ export default function JoinUsPage() {
       <Hero useParallax={false} imageUrl='https://images.unsplash.com/photo-1510146758428-e5e4b17b8b6a?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'>
         <h1>Join Us</h1>
       </Hero>
+      <Section />
       <Form />
     </div>
   )
